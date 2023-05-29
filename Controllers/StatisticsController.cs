67cefc80b4fb33d4a3106ce03b8e5b0d3de8c7ec
villeanderssonn <villeanderssonn@gmail.com>
2023-05-30 00:51:06 +0200

    Added functionality to the Overview page to view all orders in a list

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebShop.Areas.Identity.Data;
+using WebShop.Models;
+using WebShop.ViewModels.DTO;
 
 namespace WebShop.Controllers
 {
     public class StatisticsController : Controller
     {
+        private readonly ShoppingCart shoppingCart;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly AppDbContext _appDbContext;
+        public StatisticsController(AppDbContext appDbContext, UserManager<ApplicationUser> userManager, ShoppingCart shoppingCart)
+        {
+            _userManager = userManager;
+            _appDbContext = appDbContext;
+            this.shoppingCart = shoppingCart;
+
+        }
         public IActionResult OverView()
         {
-            return View();
+            // Retrieve the list of orders from the database
+            IEnumerable<OrderDTO> orders = _appDbContext.Orders.Select(o => new OrderDTO
+            {
+                FirstName = o.FirstName,
+                LastName = o.LastName,
+                City = o.City,
+                ZipCode = o.ZipCode,
+                Adress = o.Adress
+            }).ToList();
+
+            return View(orders);
         }
+
+
+
     }
 }
