2a3a5b2f681beb1a5447d9a1214b076637c68da9
Simon Frändén <simon.franden@hotmail.com>
2023-06-02 01:09:35 +0200

    *Added a service to convert sek to euro or usd

---
+++ CurrencyConverter.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+
+namespace WebShop
+{
+    public class CurrencyConverter
+    {
+        decimal euroRate = 0;
+        decimal usdRate = 0;
+
+        public decimal ConvertToEUR(decimal sek)
+        {
+            if(euroRate == 0)
+            {
+                GetExchangeRate();
+            }
+
+            return sek * euroRate;
+        }
+        public decimal ConvertToUSD(decimal sek)
+        {
+            if (usdRate == 0)
+            {
+                GetExchangeRate();
+            }
+
+            return sek * usdRate;
+        }
+
+        private async void GetExchangeRate()
+        {
+            using (var client = new HttpClient())
+            {
+                string endpoint = "https://api.freecurrencyapi.com/v1/latest?apikey=ERUkRQmP7OYvWWuBUfhiIwL1dyqxqReevmfeGvbp&currencies=EUR%2CUSD&base_currency=SEK";
+                var response = await client.GetFromJsonAsync<CurrencyResponse>(endpoint);
+
+                if (response != null)
+                {
+                    euroRate = response.data.EUR;
+                    usdRate = response.data.USD;
+                }
+            }
+            
+
+            
+        }
+    }
+
+    public class CurrencyData
+    {
+        public decimal EUR { get; set; }
+        public decimal USD { get; set; }
+    }
+
+    public class CurrencyResponse
+    {
+        public CurrencyData data { get; set; }
+    }
+}
